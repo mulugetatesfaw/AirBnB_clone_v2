e7750ef9cc4f6a6d976e5bbc7c59c3adf76d081e
mulugetatesfaw <mulugetatesfaw21@gmail.com>
2023-05-21 10:06:15 -0700

    web flask for web apps

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,7 +1,8 @@
--- Creates database hbnb_dev_db
+-- Setup for MySQL server
+-- Script to automate setup
+
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 USE hbnb_dev_db;
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost';
-SET PASSWORD FOR 'hbnb_dev'@'localhost' = 'hbnb_dev_pwd';
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
-GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+GRANT ALL ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost'
+IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT SELECT ON performance_schema.* TO hbnb_dev@localhost;
--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,7 +1,8 @@
--- Creates database hbnb_test_db
+-- Setup for MySQL server
+-- Script to automate setup
+
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 USE hbnb_test_db;
-CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost';
-SET PASSWORD FOR 'hbnb_test'@'localhost' = 'hbnb_test_pwd';
-GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
-GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+GRANT ALL ON hbnb_test_db.* TO 'hbnb_test'@'localhost'
+IDENTIFIED BY 'hbnb_test_pwd';
+GRANT SELECT ON performance_schema.* TO hbnb_test@localhost;
